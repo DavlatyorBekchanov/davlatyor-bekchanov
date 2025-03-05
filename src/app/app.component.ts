d8e28d7355962e844e686a83dafa38a04a701a0d
Davlatyor Bekchanov <klugerklug7@gmail.com>
2025-03-05 23:10:21 +0100

    header css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPagesComponent } from './main-pages/main-pages.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainPagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/main-pages/main-pages.component.ts
+++ src/app/main-pages/main-pages.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-main-pages',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './main-pages.component.html',
-  styleUrl: './main-pages.component.scss'
+  styleUrl: './main-pages.component.scss',
 })
-export class MainPagesComponent {
-
-}
+export class MainPagesComponent {}
