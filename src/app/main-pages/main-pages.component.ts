9ea13faf635d96f85c8fe258fd0e8d543f08afe2
Davlatyor Bekchanov <klugerklug7@gmail.com>
2025-03-08 10:58:52 +0100

    middle-section

--- src/app/main-pages/main-pages.component.ts
+++ src/app/main-pages/main-pages.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { LandingSectionComponent } from './landing-section/landing-section.component';
 
 @Component({
   selector: 'app-main-pages',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, LandingSectionComponent],
   templateUrl: './main-pages.component.html',
   styleUrl: './main-pages.component.scss',
 })
